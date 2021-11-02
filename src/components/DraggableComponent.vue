2cb6f15f260baedc952918dd2ac687b031818a47
rd <rd@Harshit-MacBook-Air.local>
2021-11-02 05:42:46 +0530

    draggable: ADDED code to show multiple draggables

---
+++ src/components/DraggableComponent.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <h3>{{ title }}</h3>
+  <draggable class="list-group" :list="list" @add="check" @change="checkAdd" group="people">
+    <div
+      class="list-group-item"
+      v-for="(element) in list"
+      :key="element.name"
+    >
+      {{ element.name }}
+    </div>
+  </draggable>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import draggable from "vuedraggable";
+
+export default Vue.extend({
+  name: 'DraggableComponent',
+  props:{
+    list: Array,
+    title: String
+  },
+  components: {
+    draggable,
+  },
+  data () {
+    return {
+      movestatus: true,
+    }
+  },
+  methods: {
+    checkAdd (evt) {
+      console.log(evt)
+    },
+    check (evt) {
+      console.log(evt)
+    }
+  }
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="6">
+        <DraggableComponent :title="userstitle" :list="list1"  />
+      </v-col>
+      <v-col cols="12" sm="6" md="6">
+        <v-row>
+          <v-col cols="12" sm="12" md="12">
+            <DraggableComponent :title="fetitle" :list="list2"  />
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" sm="12" md="12">
+            <DraggableComponent :title="betitle" :list="list3"  />
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" sm="12" md="12">
+            <DraggableComponent :title="qatitle" :list="list4"  />
+          </v-col>
+        </v-row>
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import DraggableComponent from "@/components/DraggableComponent"
 
 export default Vue.extend({
   name: 'Home',
   components: {
-    HelloWorld,
+    DraggableComponent
   },
+  data() {
+    return {
+      qatitle: "QA Specialist",
+      betitle: "BE Developer",
+      fetitle: "FE title",
+      userstitle: "Users",
+      list1: [
+        { name: "John", id: 1, roles: [] },
+        { name: "Joao", id: 2, roles: [] },
+        { name: "Jean", id: 3, roles: [] },
+        { name: "Gerard", id: 4, roles: [] }
+      ],
+      list2: [],
+      list3: [],
+      list4: []
+    };
+  },
+  methods: {
+    restrictUsers (role: string) {
+      if(role == this.fetitle) {
+        this.list2.splice(2)
+      }
+      else if (role == this.betitle) {
+        this.list3.splice(2)
+      }
+      else if (role == this.qatitle) {
+        this.list4.splice(2)
+      }
+    }
+  }
 });
 </script>
